284da7a49ef2126e97bbe3321728c231e7333937
Donovan <mcdonovan@outlook.fr>
2019-10-04 17:55:34 -0500

    12.2 not done

--- src/app/services/expense.service.ts
+++ src/app/services/expense.service.ts
@@ -6,13 +6,31 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ExpenseService {
-	public selectedExpense = {};
-	public expenses = [
-		{id: 1, type: "meal", description: "Dinner with a client", cost: 75},
-		{id: 2, type: "meal", description: "Lunch at airport", cost: 15},
-		{id: 1, type: "lodging", description: "Hotel at convention", cost: 200}
-	];
+	public selectedExpense: any;
+	public expenses = [];
+	ref = firebase.database().ref("expenses/");
 
-constructor() {}
+constructor() {
+	this.ref.on("value", resp => {
+		this.expenses = [];
+		this.expenses = snapshotToArray(resp);
+		console.log(this.expenses)
+	});
+}
+
+addExpense(expense) {
+	expense["user"] = firebase.auth().currentUser.email;
+	return firebase.database().ref("expenses/").push(expense);
+}
+
+export const snapshotToArray = snapshot => {
+	let returnArr = [];
+	snapshot.forEach(childSnapshot => {
+		let item = childSnapshot.val();
+		item.key = childSnapshot.key;
+		returnArr.push(item);
+	});
+	return returnArr;
+}
 
 }
