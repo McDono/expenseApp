3fc08e02076942f2420a8d7de1ee1bafced6720c
Donovan <mcdonovan@outlook.fr>
2019-10-06 19:29:34 -0500

    database problems resolved; works fine and can be sumitted for grade

--- src/app/expense-new/expense-new.page.ts
+++ src/app/expense-new/expense-new.page.ts
@@ -16,7 +16,16 @@ export class ExpenseNewPage implements OnInit {
   constructor(	private expenseService: ExpenseService,
 								private router: Router) { }
 
-  ngOnInit() {
+	ngOnInit() {
+  }
+
+  ionViewWillEnter() {
+		var form = document.querySelector("form");
+		form.addEventListener("submit", (e) => {
+			e.preventDefault();
+			console.log("Form submitted");
+			this.addExpense();
+		});
   }
 
 	addExpense() {
@@ -24,11 +33,14 @@ export class ExpenseNewPage implements OnInit {
 		console.log(this.newExpense);
 		this.expenseService.addExpense(this.newExpense)
 			.then(data => {
+				console.log("DATA : ");
 				console.log(data);
+				this.sleep(5000);
 				this.router.navigateByUrl("/list");
 			})
 			.catch(error => {
-				console.log(error);
+				// throw new Error("Something went badly wrong!");
+				console.error(error);
 			});
 	}
 
@@ -36,4 +48,12 @@ export class ExpenseNewPage implements OnInit {
 		return (this.newExpense.description == "");
 	}
 
+	sleep(milliseconds) {
+	  var start = new Date().getTime();
+	  for (var i = 0; i < 1e7; i++) {
+	    if ((new Date().getTime() - start) > milliseconds){
+	      break;
+	    }
+	  }
+	}
 }
